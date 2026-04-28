7deb3ef89664543e03111ab07be52c6191eaf2e4
Arthur Gonçalves <arthur.limag.alu.lmb@gmail.com>
2026-04-28 17:58:14 -0300

    chore: configura Flyway e adiciona migrations

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Fortaleza, CE', 'Arthur', 'Male', 'Gonçalves'),
+	(2, 'Vicoça do Ceará, Ceará', 'Paloma', 'Female', 'Nascimento'),
+	(3, 'Vicoça do Ceará, Ceará', 'João Paulo', 'Male', 'Nascimento'),
+	(5, 'Mvezo, South Africa', 'Nelson', 'Male', 'Mandela'),
+	(6, 'Smiljan, Croatia', 'Nikola', 'Male', 'Tesla');
