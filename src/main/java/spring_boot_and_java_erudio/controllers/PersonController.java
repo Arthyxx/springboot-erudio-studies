058b9052224cdeaa7455e7da02cd22c8182964f5
Arthur Gonçalves <arthur.limag.alu.lmb@gmail.com>
2026-04-29 17:26:37 -0300

    feat: adicionar documentação OpenAPI para endpoints de person

--- src/main/java/spring_boot_and_java_erudio/controllers/PersonController.java
+++ src/main/java/spring_boot_and_java_erudio/controllers/PersonController.java
@@ -1,8 +1,10 @@
 package spring_boot_and_java_erudio.controllers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import spring_boot_and_java_erudio.controllers.docs.PersonControllerDocs;
 import spring_boot_and_java_erudio.data.dto.v1.PersonDTO;
 import spring_boot_and_java_erudio.services.PersonServices;
 
@@ -10,7 +12,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/person/v1")
-public class PersonController {
+@Tag(name = "People", description = "Endpoints for Managing People")
+public class PersonController implements PersonControllerDocs {
 
     private final PersonServices service;
 
@@ -19,40 +22,45 @@ public class PersonController {
     }
 
     @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE,
-                                                MediaType.APPLICATION_XML_VALUE,
-                                                MediaType.APPLICATION_YAML_VALUE})
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE})
+    @Override
     public PersonDTO findById(@PathVariable("id") Long id){
         return service.findById(id);
     }
 
     @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE,
-                                           MediaType.APPLICATION_XML_VALUE,
-                                           MediaType.APPLICATION_YAML_VALUE})
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE})
+    @Override
     public List<PersonDTO> findAll(){
         return service.findAll();
     }
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE,
-                                            MediaType.APPLICATION_XML_VALUE,
-                                            MediaType.APPLICATION_YAML_VALUE},
-                                produces = {MediaType.APPLICATION_JSON_VALUE,
-                                            MediaType.APPLICATION_XML_VALUE,
-                                            MediaType.APPLICATION_YAML_VALUE})
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE})
+    @Override
     public PersonDTO create(@RequestBody PersonDTO person){
         return service.create(person);
     }
 
     @PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE,
-                                           MediaType.APPLICATION_XML_VALUE,
-                                           MediaType.APPLICATION_YAML_VALUE},
-                               produces = {MediaType.APPLICATION_JSON_VALUE,
-                                           MediaType.APPLICATION_XML_VALUE,
-                                           MediaType.APPLICATION_YAML_VALUE})
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE})
+    @Override
     public PersonDTO update(@RequestBody PersonDTO person){
         return service.update(person);
     }
 
     @DeleteMapping("/{id}")
+    @Override
     public ResponseEntity<?> delete(@PathVariable Long id){
         service.delete(id);
 
